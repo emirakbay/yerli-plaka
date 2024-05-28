028b4765331a6efcdd4c8f65840d8ed6bd811021
emirakbay <akbayemir@gmail.com>
2024-05-28 17:29:04 +0300

    post a record using server actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import { revalidatePath } from "next/cache";
 import { db } from "~/server/db";
+import { posts } from "~/server/db/schema";
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany({});
+  const allPosts = await db.query.posts.findMany({});
+
+  async function createPost(
+    name: string,
+    licensePlate: string,
+    description: string,
+  ) {
+    "use server";
+    try {
+      await db.insert(posts).values({
+        name: name,
+        licensePlate: licensePlate,
+        description: description,
+      });
+    } catch (error) {
+      console.error("Failed to create post", error);
+    }
+  }
 
   return (
     <main className="flex h-screen justify-center bg-gradient-to-br from-neutral-200 via-sky-100 to-gray-100">
@@ -13,19 +32,44 @@ export default async function HomePage() {
           </p>
         </div>
         <div className="pt-10">
-          <form className="flex justify-center">
+          <form
+            className="flex justify-center"
+            action={async (e: FormData) => {
+              "use server";
+              await createPost(
+                e.get("name") as string,
+                e.get("licensePlate") as string,
+                e.get("description") as string,
+              );
+              revalidatePath("/");
+            }}
+          >
             <input
+              className="rounded-l border border-gray-300 p-2"
+              name="name"
+              placeholder="Name"
               type="text"
-              placeholder="Plaka numarası girin"
-              className="rounded-l border border-gray-400 p-2"
             />
-            <button className="rounded-r bg-blue-500 p-2 text-white">
-              Sorgula
-            </button>
+            <input
+              className="border border-gray-300 p-2"
+              name="licensePlate"
+              placeholder="License Plate"
+              type="text"
+            />
+            <input
+              className="border border-gray-300 p-2"
+              name="description"
+              placeholder="Description"
+              type="text"
+            />
+            <button
+              className="rounded-r bg-blue-500 p-2 text-white"
+              type="submit"
+            ></button>
           </form>
 
           <ul className="mx-auto grid w-fit grid-cols-2 gap-x-40 gap-y-5 pt-5">
-            {posts.map((post) => (
+            {allPosts.map((post) => (
               <li key={post.id} className="rounded border border-gray-300 p-2">
                 <h2 className="text-xl font-bold">{post.name}</h2>
                 <p>{post.licensePlate}</p>
