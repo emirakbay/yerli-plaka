4988d65285f02b92fd60a3a458dd644d7dde3c22
Emir Akbay <akbayemir@gmail.com>
2024-05-28 03:14:02 +0300

    add mock ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,35 @@
-import Link from "next/link";
+import { mockData } from "~/server/db/mock-data";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <main className="flex h-screen justify-center bg-gradient-to-br from-neutral-200 via-sky-100 to-gray-100">
+      <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
+        <div>
+          <h1 className="pt-2 text-center text-3xl font-bold">Yerli Plaka</h1>
+          <p className="text-center text-sm text-gray-500">
+            Yerli plaka numarası sorgulama
+          </p>
+        </div>
+        <div className="pt-10">
+          <form className="flex justify-center">
+            <input
+              type="text"
+              placeholder="Plaka numarası girin"
+              className="rounded-l border border-gray-400 p-2"
+            />
+            <button className="rounded-r bg-blue-500 p-2 text-white">
+              Sorgula
+            </button>
+          </form>
+
+          <ul className="mx-auto grid w-fit grid-cols-2 gap-x-40 gap-y-5 pt-5">
+            {mockData.map((post) => (
+              <li key={post.id} className="border-b border-black">
+                <h2 className="text-lg font-bold">{post.title}</h2>
+                <p>{post.content}</p>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </main>
---
+++ src/server/db/mock-data.ts
@@ -0,0 +1,103 @@
+export const mockData = [
+  {
+    id: 1,
+    title: "First Post",
+    content: "Hello World",
+  },
+  {
+    id: 2,
+    title: "Second Post",
+    content: "More content",
+  },
+  {
+    id: 3,
+    title: "Third Post",
+    content: "Even more content",
+  },
+
+  {
+    id: 4,
+    title: "Fourth Post",
+    content: "Even more content",
+  },
+  {
+    id: 5,
+    title: "Fifth Post",
+    content: "Even more content",
+  },
+  {
+    id: 6,
+    title: "Sixth Post",
+    content: "Even more content",
+  },
+  {
+    id: 7,
+    title: "Seventh Post",
+    content: "Even more content",
+  },
+  {
+    id: 8,
+    title: "Eighth Post",
+    content: "Even more content",
+  },
+  {
+    id: 9,
+    title: "Ninth Post",
+    content: "Even more content",
+  },
+  {
+    id: 10,
+    title: "Tenth Post",
+    content: "Even more content",
+  },
+  {
+    id: 11,
+    title: "Eleventh Post",
+    content: "Even more content",
+  },
+  {
+    id: 12,
+    title: "Twelveth Post",
+    content: "Even more content",
+  },
+  {
+    id: 13,
+    title: "Thirteenth Post",
+    content: "Even more content",
+  },
+  {
+    id: 14,
+    title: "Fourteenth Post",
+    content: "Even more content",
+  },
+  {
+    id: 15,
+    title: "Fifteenth Post",
+    content: "Even more content",
+  },
+  {
+    id: 16,
+    title: "Sixteenth Post",
+    content: "Even more content",
+  },
+  {
+    id: 17,
+    title: "Seventeenth Post",
+    content: "Even more content",
+  },
+  {
+    id: 18,
+    title: "Eighteenth Post",
+    content: "Even more content",
+  },
+  {
+    id: 19,
+    title: "Nineteenth Post",
+    content: "Even more content",
+  },
+  {
+    id: 20,
+    title: "Twentieth Post",
+    content: "Even more content",
+  },
+];
